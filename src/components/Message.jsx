7c79c6960cc82ccf02ff1b510bb5f6763c7566e9
Ishank Bansal <ishankbansal1111@gmail.com>
2022-11-30 23:32:37 +0530

    message

---
+++ src/components/Message.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Message = () => {
+    return (
+        <div className="message owner">
+            <div className="messageInfo">
+                <img src="https://i0.wp.com/calmatters.org/wp-content/uploads/2022/05/Senior-Grads-Zintan-CJN-CM-02.jpg?fit=1200%2C800&ssl=1"/>
+                <span>Just Now</span>
+            </div>
+            <div className="messageContent">
+                <p>Hello</p>
+                <img src="https://i0.wp.com/calmatters.org/wp-content/uploads/2022/05/Senior-Grads-Zintan-CJN-CM-02.jpg?fit=1200%2C800&ssl=1"/>
+            </div>
+        </div>
+    )
+}
+
+export default Message
