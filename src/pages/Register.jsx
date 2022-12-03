78b012c9671f017fd08a15d57c33a8352aa2ef5e
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-03 23:26:59 +0530

    register changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Add from "../img/addAvatar.png"
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase"
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
+
 
 const Register = () => {
+    const [err, setErr] = useState(false)
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        const displayName = e.target[0].value;
+        const email = e.target[1].value;
+        const password = e.target[2].value;
+        const file = e.target[3].files[0];
+
+        try{
+            const res = await createUserWithEmailAndPassword(auth, email, password)
+
+            const storageRef = ref(storage, displayName);
+
+            const uploadTask = uploadBytesResumable(storageRef, file);
+
+            uploadTask.on('state_changed', 
+                (error) => {
+                    setErr(true)
+                }, 
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) => {
+                        await updateProfile(res.user, {
+                            displayName,
+                            photoURL: downloadURL,
+                        });
+                        await setDoc(doc(db, "users", res.user.uid), {
+                            uid: res.user.uid,
+                            displayName,
+                            email,
+                            photoURL: downloadURL
+                        })
+                    });
+                }
+            );
+
+        }catch(err){
+            setErr(true);
+        }
+
+    }
+
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">Fly Chat</span>
                 <span className="title">Register</span>
-                <form>
+                <form onSubmit = {handleSubmit}>
                     <input type="text" placeholder="display name"/>
                     <input type="email" placeholder="email"/>
                     <input type="password" placeholder="password"/>
@@ -17,6 +63,7 @@ const Register = () => {
                         <span>Add an Avatar</span>
                     </label>
                     <button>Sign Up</button>
+                    {err && <span>Something went wrong</span>}
                 </form>
                 <p>You do have a account? Login</p>
             </div>
