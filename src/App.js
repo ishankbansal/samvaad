9e6893fa31f4180294a2dcc556e6d6233d259877
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-05 23:00:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import './style.scss'
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 function App(){
+
+    const {currentUser} = useContext(AuthContext)
+
+    console.log(currentUser);
     return(
-        <div>
-            <Register/>
-        </div>
-        
+        <BrowserRouter>
+            <Routes>
+                <Route index element = {<Home/>}/>
+                <Route path= 'login' element = {<Login/>}/>
+                <Route path= 'register' element = {<Register/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
