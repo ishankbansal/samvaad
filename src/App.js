6cade0c9b15d02557b52c5a6b66b4325385ce681
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-07 21:27:49 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Register from './pages/Register'
 import Login from './pages/Login'
 import './style.scss'
 import Home from './pages/Home';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
 
@@ -10,11 +10,22 @@ function App(){
 
     const {currentUser} = useContext(AuthContext)
 
-    console.log(currentUser);
+    const ProtectedRoute = ({children}) => {
+        if(!currentUser){
+            return <Navigate to='/login'/>
+        }
+
+        return children
+    }
+
     return(
         <BrowserRouter>
             <Routes>
-                <Route index element = {<Home/>}/>
+                <Route index element = {
+                    <ProtectedRoute>
+                        <Home/>
+                    </ProtectedRoute>
+                }/>
                 <Route path= 'login' element = {<Login/>}/>
                 <Route path= 'register' element = {<Register/>}/>
             </Routes>
