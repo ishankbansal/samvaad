94adc0feb4396c23669ec09428d126c44f1e77e4
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-08 19:13:38 +0530

    update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, Link } from 'react-router-dom';
 import Add from "../img/addAvatar.png"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../firebase';
+
 
 const Login = () => {
+    const [err, setErr] = useState(false)
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        console.log(e);
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        try{
+            await signInWithEmailAndPassword(auth, email, password)
+            navigate("/")
+        }catch(err){
+            setErr(true);
+        }
+
+    }
+
     return (
         <div className="formContainer">
             <div className="formWrapper">
                 <span className="logo">Fly Chat</span>
                 <span className="title">Login</span>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="email" placeholder="email"/>
                     <input type="password" placeholder="password"/>
                     <button>Sign In</button>
+                    {err && <span>Something went wrong</span>}
                 </form>
-                <p>You don't have a account? Register</p>
+                <p>You don't have a account? <Link to = "/register">Register</Link></p>
             </div>
         </div>
     )
