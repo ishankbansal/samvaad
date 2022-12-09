3e6544edacb1fffb9eeee16fba9e04ad05eaf6fb
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-09 23:39:05 +0530

    update

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,29 +4,39 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, db, storage } from "../firebase"
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore"; 
+import { Link, useNavigate } from "react-router-dom"
 
 
 const Register = () => {
     const [err, setErr] = useState(false)
+    const navigate = useNavigate();
+
     const handleSubmit = async (e) =>{
         e.preventDefault();
+        console.log(e);
         const displayName = e.target[0].value;
         const email = e.target[1].value;
         const password = e.target[2].value;
         const file = e.target[3].files[0];
 
         try{
+            console.log("exe");
             const res = await createUserWithEmailAndPassword(auth, email, password)
-
+            console.log("exe1");
             const storageRef = ref(storage, displayName);
-
+            console.log("exe2");
+            
             const uploadTask = uploadBytesResumable(storageRef, file);
+            console.log("exe3");
 
             uploadTask.on('state_changed', 
-                (error) => {
-                    setErr(true)
-                }, 
+                // (error) => {
+                //     console.log(error);
+                //     setErr(true)
+                //     console.log("error1");
+                // }, 
                 () => {
+                    console.log("exe4");
                     getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) => {
                         await updateProfile(res.user, {
                             displayName,
@@ -38,12 +48,16 @@ const Register = () => {
                             email,
                             photoURL: downloadURL
                         })
+
+                        await setDoc(doc(db, "userChats", res.user.uid), {})
+                        navigate("/");
                     });
                 }
             );
 
         }catch(err){
             setErr(true);
+            console.log("error2");
         }
 
     }
@@ -65,7 +79,7 @@ const Register = () => {
                     <button>Sign Up</button>
                     {err && <span>Something went wrong</span>}
                 </form>
-                <p>You do have a account? Login</p>
+                <p>You do have a account? <Link to = "/login">Login</Link></p>
             </div>
         </div>
     )
