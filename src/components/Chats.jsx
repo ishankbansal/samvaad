58297caf5e84451c0ddca04e18383d592c99c5af
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-10 22:54:11 +0530

    chats update

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { db } from '../firebase';
 
 const Chats = () => {
+
+    const [chats, setChats] = useState([]);
+
+    const {currentUser} = useContext(AuthContext);
+
+    useEffect(() => {
+        const getChats = () => {
+            const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+                setChats(doc.data());
+            });        
+
+            return () => {
+                unsub();
+            }
+        }
+
+        currentUser.uid && getChats();
+    },[currentUser.uid])
+
+    console.log(Object.entries(chats));
+
     return (
         <div className="chats">
             <div className="userChat">
