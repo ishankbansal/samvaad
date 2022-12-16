edb0072f8e09047728c33d4fb493c4e47c51a4f5
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-16 23:22:03 +0530

    input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Img from "../img/img.png"
 import Attach from "../img/attach.png"
+import { AuthContext } from '../context/AuthContext';
+import { ChatContext } from '../context/ChatContext';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { db } from '../firebase';
 
 const Input = () => {
+
+    const [text, setText] = useState("");
+    const [img, setImg] = useState("");
+
+    const {currentUser} = useContext(AuthContext);
+    const {data} = useContext(ChatContext);
+
+    const handleSend = async() => {
+        if(img){
+
+        }
+        else{
+            await updateDoc(doc(db, "chats", data.chatId),{
+                messages: arrayUnion({
+                    
+                })
+            })
+        }
+    }
+
     return (
         <div className="input">
-            <input type="text" placeholder="Type Something..."/>
+            <input type="text" placeholder="Type Something..." onChange={e=>setText(e.target.value)}/>
             <div className="send">
                 <img src={Attach}/>
-                <input type="file" style={{display: "none"}} id = "file"/>
+                <input type="file" style={{display: "none"}} id = "file" onChange={e=>setImg(e.target.files[0])}/>
                 <label htmlFor = "file">
                     <img src={Img} />
                 </label>
-                <button>Send</button>
+                <button onClick={handleSend}>Send</button>
             </div>
         </div>
     )
