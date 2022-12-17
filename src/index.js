bdbcc8e4e88d76837dc0d6effec8092313925920
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-17 17:25:09 +0530

    index file update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
+import { ChatContextProvider } from "./context/ChatContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <AuthContextProvider>
-        <App/>
+        <ChatContextProvider>
+            <App/>
+        </ChatContextProvider>
     </AuthContextProvider>
 );
