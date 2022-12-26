c6bd0f54d7a2d5d9bc7bdb234c9bcd71904c0a4f
Ishank Bansal <ishankbansal1111@gmail.com>
2022-12-26 21:49:38 +0530

    added date

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect, useRef } from 'react'
 import { AuthContext } from '../context/AuthContext';
 import { ChatContext } from '../context/ChatContext';
+import { format } from "date-fns"
 
 const Message = ({message}) => {
 
@@ -21,11 +22,12 @@ const Message = ({message}) => {
         >
             <div className="messageInfo">
                 <img src={message.senderID === currentUser.uid ? currentUser.photoURL : data.user.photoURL}/>
-                {/* <span>Just Now</span> */}
             </div>
             <div className="messageContent">
-                <p>{message.text}</p>
+                {message.text !== "" && <p>{message.text}</p>}
                 {message.img && <img src={message.img}/>}
+                {/* <span>{message.date.toDate().toDateString()}</span> */}
+                <span>{message.date.toDate().toLocaleString('en-US', { timeZone: 'IST' })}</span>
             </div>
         </div>
     )
