d81f8560b988762fad1850d83dc9492a635019f9
Ishank Bansal <ishankbansal1111@gmail.com>
2023-01-08 23:48:27 +0530

    update

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,17 +1,21 @@
-import React, { useContext } from 'react'
-import Cam from '../img/cam.png'
-import Add from '../img/add.png'
-import More from '../img/more.png'
-import Messages from './Messages'
-import Input from './Input'
-import { ChatContext } from '../context/ChatContext'
-
+import React, { useContext } from "react";
+import Cam from "../img/cam.png";
+import Add from "../img/add.png";
+import More from "../img/more.png";
+import Messages from "./Messages";
+import Input from "./Input";
+import { ChatContext } from "../context/ChatContext";
 
 const Chat = () => {
-    const {data} = useContext(ChatContext);
+  const { data } = useContext(ChatContext);
+  console.log(Object.keys(data.user).length);
+  return (
+    <div className="chat">
+      {Object.keys(data.user).length === 0 && (
+        <div>Choose a user from your chat section or search for a user :)</div>
+      )}
 
-    return (
-      <div className="chat">
+      {Object.keys(data.user).length !== 0 && (
         <div className="chatInfo">
           <div className="container">
             <img src={data.user?.photoURL} />
@@ -23,10 +27,11 @@ const Chat = () => {
                     <img src={More} />
                 </div> */}
         </div>
-        <Messages />
-        <Input />
-      </div>
-    );
-}
+      )}
+      {Object.keys(data.user).length !== 0 && <Messages />}
+      {Object.keys(data.user).length !== 0 && <Input />}
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
