ce0d60b178da93f40342eb5ada70eddd3f6346e2
Ishank Bansal <ishankbansal1111@gmail.com>
2023-01-15 23:58:08 +0530

    update

--- src/components/AllUser.jsx
+++ src/components/AllUser.jsx
@@ -1,65 +1,42 @@
-import React, { useEffect, useState } from 'react'
-import { db } from '../firebase';
+import React, { useEffect, useState } from "react";
+import { db } from "../firebase";
 import { collection, getDocs, query } from "firebase/firestore";
 
 const AllUser = () => {
+  const [allUsers, setAllUsers] = useState([]);
 
-    const [allUsers, setAllUsers] = useState([]);
+  const handleSearch = async () => {
+    const q = query(collection(db, "users"));
+    const getUsers = [];
 
-    const handleSearch = async () => {
-      const q = query(
-        collection(db, "users"),
-      );
-      const getUsers = [];
-
-      try {
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-          getUsers.push({
-            ...doc.data(),
-            key: doc.id,
-          });
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        getUsers.push({
+          ...doc.data(),
+          key: doc.id,
         });
-        setAllUsers(getUsers);
-      } catch (err) {
+      });
+      setAllUsers(getUsers);
+    } catch (err) {}
+  };
 
-      }
-    };
+  useEffect(() => {
+    handleSearch();
+  }, []);
 
-    // useEffect(() => {
-    //     const q = query(
-    //       collection(db, "users"),
-    //     );
+  return (
+    <div>
+      {allUsers.map((user) => (
+        <div key={user.key} className="userChat">
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-    //     try{
-    //         const querySnapshot
-    //         (async () => {
-    //             querySnapshot = await getDocs(q);
-    //         })
-    //         querySnapshot.forEach((doc) => {
-    //             getUsers.push({
-    //                 ...doc.data()
-    //             })
-    //         });
-    //         setAllUsers(getUsers);
-    //     }
-    //     catch(err){
-
-    //     }
-
-    // },[])
-
-    useEffect(()=>{
-        handleSearch();
-    },[])
-
-    return (
-      <div>
-        {allUsers.map((user) => (
-          <div key={user.key}>{user.displayName}</div>
-        ))}
-      </div>
-    );
-}
-
-export default AllUser
+export default AllUser;
