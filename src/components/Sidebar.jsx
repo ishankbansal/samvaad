7fd567edc969cbb97aca42ec1f63c236052475df
Ishank Bansal <ishankbansal1111@gmail.com>
2023-02-14 23:42:01 +0530

    update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Chats from './Chats'
-import Navbar from './Navbar'
-import Search from './Search'
+import React from "react";
+import Chats from "./Chats";
+import Navbar from "./Navbar";
+import Search from "./Search";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar">
-            <Navbar/>
-            <Search/>
-            <Chats/>
-        </div>
-    )
-}
+  return (
+    <div className="sidebar">
+      <Navbar />
+      <Search />
+      <Chats />
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
