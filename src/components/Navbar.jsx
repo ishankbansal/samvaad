b724e73f32b9db397ba247eec913b1025b541c7e
Ishank Bansal <ishankbansal1111@gmail.com>
2023-02-27 19:50:25 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,15 +6,15 @@ import { auth } from '../firebase'
 const Navbar = () => {
     const {currentUser} = useContext(AuthContext);
     return (
-        <div className="navbar">
-            <span className="logo">Fly Chat</span>
-            <div className="user">
-                <img src={currentUser.photoURL} alt=""/>
-                <span>{currentUser.displayName}</span>
-                <button onClick={() => signOut(auth)}>Logout</button>
-            </div>
+      <div className="navbar">
+        <span className="logo">samवाद</span>
+        <div className="user">
+          <img src={currentUser.photoURL} alt="" />
+          <span>{currentUser.displayName}</span>
+          <button onClick={() => signOut(auth)}>Logout</button>
         </div>
-    )
+      </div>
+    );
 }
 
 export default Navbar
